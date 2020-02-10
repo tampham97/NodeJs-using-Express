bde47ae791184c65c19daa5a5784d0895a2d6a0e
tampham97 <phamthanhtam10697@gmail.com>
2020-02-10 13:31:35 +0700

    server side validation

--- index.js
+++ index.js
@@ -12,11 +12,12 @@ app.set('views', './views')
 // middleware cua body-parser
 app.use(bodyParser.json()) // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
-
-app.use('/users', userRoute)
 app.get('/', function(req, res){
     res.render("index",{name: 'Tam Pham'})
 })
+
+app.use('/users', userRoute)
+
 
 app.listen(port, function(){
     console.log("port is running....")
