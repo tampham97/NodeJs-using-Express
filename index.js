0ab5e5cee57f5a850aa8a2d5c0e87cbf61622ba2
tampham97 <phamthanhtam10697@gmail.com>
2020-02-10 10:27:21 +0700

    add lowdb

--- index.js
+++ index.js
@@ -2,31 +2,35 @@ var express = require('express');
 var app = express();
 const bodyParser = require('body-parser')
 var port = 3000;
+//lowdb
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+const adapter = new FileSync('db.json')
+const db = low(adapter)
+db.defaults({ users: [] })
+  .write()
+
 //goi method cua pug
 app.set('view engine', 'pug');
 app.set('views', './views')
 // middleware cua body-parser
 app.use(bodyParser.json()) // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
-var users=  [
-    {id: 1, name : "Tam"},
-    {id: 2, name : "Ti"},
-    {id: 3, name : "Teo"}
-]
+
 
 app.get('/', function(req, res){
     res.render("index",{name: 'Tam Pham'})
 })
 app.get('/users', function(req, res){
     res.render("users/index", {
-        users : users
+        users : db.get("users").value()
     })
 })
 app.get('/users/search', function(req,res){
     var q = req.query.q;
-    var matchUsers = users.filter(function(user){
+    var matchUsers = db.get("users").filter(function(user){
         return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
-    });
+    }).write();
     res.render("users/index", {
         users : matchUsers
     })
@@ -36,7 +40,7 @@ app.get('/users/create',function(req, res){
 })
 //nhan data tu 
 app.post('/users/create',function(req, res){
-    users.push(req.body);
+    db.get("users").push(req.body).write();
     res.redirect('/users')
 })
 app.listen(port, function(){
