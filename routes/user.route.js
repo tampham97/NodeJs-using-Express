20311b09112894dc2645b5d8f9d3405be1cad326
tampham97 <phamthanhtam10697@gmail.com>
2020-02-10 13:48:28 +0700

    middleware

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,15 +1,15 @@
 var express = require('express')
 var router = express.Router();
 
-
+var validate = require('../validation/user.validate')
 
 var controller = require('../controllers/user.controller')
 
 router.get('/', controller.index);
 
 router.get('/search', controller.search);
-router.get('/create', controller.create);
+router.get('/create',controller.create);
 router.get('/:id', controller.get);
 //nhan data bang post
-router.post('/create',controller.postCreate);
+router.post('/create',validate.postCreate, controller.postCreate);
 module.exports = router
