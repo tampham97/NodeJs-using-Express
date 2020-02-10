b78ac5ef57cadbc2f818060c67f0c8094a932ef9
tampham97 <phamthanhtam10697@gmail.com>
2020-02-10 09:44:07 +0700

    res.query

--- index.js
+++ index.js
@@ -6,17 +6,27 @@ var port = 3000;
 app.set('view engine', 'pug');
 app.set('views', './views')
 
+var users=  [
+    {id: 1, name : "Tam"},
+    {id: 2, name : "Ti"},
+    {id: 3, name : "Teo"}
+]
 
 app.get('/', function(req, res){
     res.render("index",{name: 'Tam Pham'})
 })
 app.get('/users', function(req, res){
     res.render("users/index", {
-        users: [
-            {id: 1, name : "Tam"},
-            {id: 2, name : "Ti"},
-            {id: 3, name : "Teo"}
-        ]
+        users : users
+    })
+})
+app.get('/users/search', function(req,res){
+    var q = req.query.q;
+    var matchUsers = users.filter(function(user){
+        return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
+    });
+    res.render("users/index", {
+        users : matchUsers
     })
 })
 app.listen(port, function(){
