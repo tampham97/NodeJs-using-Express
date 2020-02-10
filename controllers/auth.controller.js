143f85cfa070fcaee6421551afade694cb9a0f62
tampham97 <phamthanhtam10697@gmail.com>
2020-02-10 15:36:22 +0700

    hashedPassword with md5

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,5 +1,5 @@
 var db = require('../db');
-
+var md5 = require('md5')
 module.exports.login = function(req, res){
     res.render("auth/login")
 }
@@ -14,8 +14,9 @@ module.exports.postLogin = function(req,res){
         });
         return;
     }
-    if(user.password !== password){
-        res.render('auth/login', {
+    var hashedPassword = md5(password)
+    if(user.password !== hashedPassword){
+        res.render('auth/login', {   
             errors : ["Wrong Password."],
             values :req.body
         });
