93b225d73864f318ad6a58f128ae860f3bf79c7f
tampham97 <phamthanhtam10697@gmail.com>
2020-02-10 09:06:33 +0700

    template engine: pug

--- index.js
+++ index.js
@@ -2,9 +2,22 @@ var express = require('express');
 
 var app = express();
 var port = 3000;
+//goi method cua pug
+app.set('view engine', 'pug');
+app.set('views', './views')
+
 
 app.get('/', function(req, res){
-    res.send("Hello world!")
+    res.render("index",{name: 'Tam Pham'})
+})
+app.get('/users', function(req, res){
+    res.render("users/index", {
+        users: [
+            {id: 1, name : "Tam"},
+            {id: 2, name : "Ti"},
+            {id: 3, name : "Teo"}
+        ]
+    })
 })
 app.listen(port, function(){
     console.log("port is running....")
