9a60370dc3ec9c2ec3fc5ad1b49d416cedd67f27
tampham97 <phamthanhtam10697@gmail.com>
2020-02-10 10:01:49 +0700

    post method

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 var express = require('express');
-
 var app = express();
+const bodyParser = require('body-parser')
 var port = 3000;
 //goi method cua pug
 app.set('view engine', 'pug');
 app.set('views', './views')
-
+// middleware cua body-parser
+app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 var users=  [
     {id: 1, name : "Tam"},
     {id: 2, name : "Ti"},
@@ -28,6 +30,14 @@ app.get('/users/search', function(req,res){
     res.render("users/index", {
         users : matchUsers
     })
+})
+app.get('/users/create',function(req, res){
+    res.render('users/create')
+})
+//nhan data tu 
+app.post('/users/create',function(req, res){
+    users.push(req.body);
+    res.redirect('/users')
 })
 app.listen(port, function(){
     console.log("port is running....")
